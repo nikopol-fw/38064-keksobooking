123f206d0f44ac462cad5d167ada8c1a0757ad2c
Nikolay Dumchev <nikolay.dumchev@gmail.com>
2018-09-23 04:13:25 +0300

    Рефакторит код, исправляет ошибки eslint, добавляет комментарии ко всем функциям

--- js/map.js
+++ js/map.js
@@ -42,12 +42,16 @@ var PHOTOS = [
 var Y_BALLOON_CHOORDINATES = [130, 630];
 var BALLOON_WIDTH = 50;
 var BALLOON_HEIGHT = 70;
+
 var AD_NUMBER = 8;
 var AVATAR_NUMBER = 8;
+
 var MIN_PRICE = 1000;
 var MAX_PRICE = 1000000;
+
 var MIN_ROOMS = 1;
 var MAX_ROOMS = 5;
+
 var MIN_GUESTS = 1;
 var MAX_GUESTS = 100;
 
@@ -56,7 +60,7 @@ for (var i = 1; i <= AVATAR_NUMBER; i++) {
   avatarsList.push(i);
 }
 
-var titlesList = REALTY_TITLES;
+var titlesList = REALTY_TITLES.slice();
 
 var map = document.querySelector('.map');
 var pinMap = map.querySelector('.map__pins');
@@ -64,31 +68,30 @@ var filterMap = map.querySelector('.map__filters-container');
 
 
 // Возвращает итератор случайного элемента массива
-// @array - массив с элементами для выборки
-var getIteratorRandomItemArray = function (arrayArg) {
+// @arrayArg - массив с элементами для выборки
+var getRandomIteratorArray = function (arrayArg) {
   return Math.floor(Math.random() * arrayArg.length);
 };
 
 // Возвращает случайный элемент из массива, удаляя его из массива
 // @iterator - целое число, итератор элемента массива для извлечения
 // @arrayToSplice - массив из которого будет выбираться случайный элемент
-var spliceRandomItem = function(iterator, arrayToSplice) {
+var spliceItemArray = function (iterator, arrayToSplice) {
   return arrayToSplice.splice(iterator, 1)[0];
 };
 
 // Возвращает случайное значение между min и max с равномерным распределением
 // @min - целое число, минимальная граница диапазона для выборки случайного числа
 // @max - целое число, максимальная граница
-var genericNumberBetweenMinMax = function(min, max) {
+var getRandomBetweenMinMax = function (min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1));
 };
 
-
 // Возвращает url для Аватарки
-// @listOfAvatars - массив с номерами картинок аватаров (массив будет изменяться функцией)
-var genericAvatar = function (listOfAvatars) {
-  var iterator = getIteratorRandomItemArray(listOfAvatars);
-  var imgUrl = spliceRandomItem(iterator, listOfAvatars);
+// @listOfAvatars - массив с номерами картинок аватаров (массив будет изменяться функцией, splice)
+var getAvatar = function (listOfAvatars) {
+  var iterator = getRandomIteratorArray(listOfAvatars);
+  var imgUrl = spliceItemArray(iterator, listOfAvatars);
   imgUrl.toString();
 
   if ((iterator + 1) < 10) {
@@ -100,42 +103,35 @@ var genericAvatar = function (listOfAvatars) {
 };
 
 // Возвращает случайную строку-заголовок для объявления из массива заголовков
-// @listOfTitles - массив с названиями (массив будет изменяться функцией)
-var genericTitle = function (listOfTitles) {
-  var iterator = getIteratorRandomItemArray(titlesList);
-  var newTitle = spliceRandomItem(iterator, titlesList);
+// @listOfTitles - массив с названиями (массив будет изменяться функцией, splice)
+var getTitle = function (listOfTitles) {
+  var iterator = getRandomIteratorArray(listOfTitles);
+  var newTitle = spliceItemArray(iterator, listOfTitles);
 
   return newTitle;
 };
 
-//var widthPin = document
-// Возвращает случайное значение от 1 до ширины переданного блока
-// @container - domNode ширина которого будет приниматься за максимальную x-координату
+// Возвращает случайное значение от 1 до ширины переданного блока (получение x-координаты для balloon)
+// @container - domElement ширина которого будет приниматься за максимальную x-координату
 var getBalloonXCoords = function (container) {
   var containerWidth = container.clientWidth;
-  var newCoord = genericNumberBetweenMinMax(1, containerWidth);
+  var newCoord = getRandomBetweenMinMax(1, containerWidth);
   newCoord -= BALLOON_WIDTH / 2;
   return newCoord;
-};
-
-// Возвращает случайное значение типа недвижимости из массива констант типов недвижимости
-var genericRealtyType = function() {
-  //return getRandomArrayItem(REALTY_TYPES);
 };
 
 // Возвращает новый массив указанной длинны с перемешанными случайным образом элементами переданного массива
 // Должно выполняться условие: arrayToSort.length >= number, иначе функция вернет -1
 // @arrayToSort - массив для сортировки (меремешивания)
+// @number - число, количество элементов в новом массиве
 var sortRandomArray = function (arrayToSort, number) {
   if (number > arrayToSort.length) {
     return -1;
   } else {
     var unsortedArray = arrayToSort.slice();
-
     var sortedArray = [];
-    for (var i = 0; i < number; i++) {
-      var iterator = getIteratorRandomItemArray(unsortedArray);
-
+    for (var j = 0; j < number; j++) {
+      var iterator = getRandomIteratorArray(unsortedArray);
       sortedArray.push(unsortedArray.splice(iterator, 1)[0]);
     }
 
@@ -143,28 +139,24 @@ var sortRandomArray = function (arrayToSort, number) {
   }
 };
 
-// Возвращает новый массив случайного размера от 1 до размера переданного массива со случайным набором неповторяющихся элементов из переданного массива
-// @features - массив, из которого нужно сделать случайную выборку случайного размера
-var genericFeatures = function (features) {
-  //var featuresNumber = genericNumberBetweenMinMax(1, features.length);
-  //var newFeatures = sortRandomArray(features, featuresNumber);
-
-  return newFeatures;
-};
-
+// Возвращает объект с информацией о новом объявлении
+// @listOfAvatars - массив с номерами от 1 до количества картинок с аватарами для генерации url'а картинки (массив будет уменьшаться на извелеченный элемент, splice)
+// @listOfTitles - массив строк с названиями объявлений (массив будет уменьшаться на извелеченный элемент, splice)
+// @balloonPlace - domElement в котором будут располагаться balloons
+// @balloonCoords - двумерный массив с верхней и нижней y-координатами balloons
 var createAd = function (listOfAvatars, listOfTitles, balloonPlace, balloonCoords) {
-  var newAvatar = genericAvatar(listOfAvatars);
-  var newTitle = genericTitle(listOfTitles);
+  var newAvatar = getAvatar(listOfAvatars);
+  var newTitle = getTitle(listOfTitles);
   var xLocation = getBalloonXCoords(balloonPlace);
-  var yLocation = genericNumberBetweenMinMax(balloonCoords[0], balloonCoords[1]) - BALLOON_HEIGHT;
+  var yLocation = getRandomBetweenMinMax(balloonCoords[0], balloonCoords[1]) - BALLOON_HEIGHT;
   var newAddress = xLocation.toString() + ', ' + yLocation.toString();
-  var newPrice = genericNumberBetweenMinMax(MIN_PRICE, MAX_PRICE);
-  var newType = REALTY_TYPES[getIteratorRandomItemArray(REALTY_TYPES)];
-  var newRooms = genericNumberBetweenMinMax(MIN_ROOMS, MAX_ROOMS);
-  var newGuests = genericNumberBetweenMinMax(MIN_GUESTS, MAX_GUESTS);
-  var newCheckin = CHECK_IN_OUT[getIteratorRandomItemArray(CHECK_IN_OUT)];
-  var newCheckout = CHECK_IN_OUT[getIteratorRandomItemArray(CHECK_IN_OUT)];
-  var newFeatures = sortRandomArray(FEATURES, getIteratorRandomItemArray(FEATURES));
+  var newPrice = getRandomBetweenMinMax(MIN_PRICE, MAX_PRICE);
+  var newType = REALTY_TYPES[getRandomIteratorArray(REALTY_TYPES)];
+  var newRooms = getRandomBetweenMinMax(MIN_ROOMS, MAX_ROOMS);
+  var newGuests = getRandomBetweenMinMax(MIN_GUESTS, MAX_GUESTS);
+  var newCheckin = CHECK_IN_OUT[getRandomIteratorArray(CHECK_IN_OUT)];
+  var newCheckout = CHECK_IN_OUT[getRandomIteratorArray(CHECK_IN_OUT)];
+  var newFeatures = sortRandomArray(FEATURES, getRandomIteratorArray(FEATURES));
   var newDescription = '';
   var newPhotos = sortRandomArray(PHOTOS, PHOTOS.length);
 
@@ -194,18 +186,22 @@ var createAd = function (listOfAvatars, listOfTitles, balloonPlace, balloonCoord
   return newAd;
 };
 
+
+// Создаем массив объявлений
 var advertisements = [];
-for (var i = 0; i < AD_NUMBER; i++) {
-  advertisements[i] = createAd(avatarsList, titlesList, pinMap, Y_BALLOON_CHOORDINATES);
+for (i = 0; i < AD_NUMBER; i++) {
+  advertisements.push(createAd(avatarsList, titlesList, pinMap, Y_BALLOON_CHOORDINATES));
 }
 
 
-//
+// Возвразает domElement balloon'а
+// @template - node (разметка balloon'а)
+// @advert - объект типа объявление, созданный функцией createAd
 var createAdvertisementNode = function (template, advert) {
   var newNode = template.cloneNode(true);
   var newNodeImg = newNode.querySelector('img');
 
-  newNode.style.left = advert.location.x.toString()  + 'px';
+  newNode.style.left = advert.location.x.toString() + 'px';
   newNode.style.top = advert.location.y.toString() + 'px';
 
   newNodeImg.src = advert.author.avatar;
@@ -214,17 +210,19 @@ var createAdvertisementNode = function (template, advert) {
   return newNode;
 };
 
-var balloonTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
+// Получаем шаблон balloon'а
+// Создаем фрагмент. Наполняем его элементами и размещаем в контейнере
+var balloonTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var balloonFragmen = document.createDocumentFragment();
 for (i = 0; i < advertisements.length; i++) {
   balloonFragmen.appendChild(createAdvertisementNode(balloonTemplate, advertisements[i]));
 }
-
 pinMap.appendChild(balloonFragmen);
 
 
-//
+// Возвращаем domElement <li> с соответствующим удобству css-классом
+// @feature - строка, название удобства из списка FEATURES
 var createFeaturesItemNode = function (feature) {
   var item = document.createElement('li');
   item.classList.add('popup__feature');
@@ -257,7 +255,8 @@ var createFeaturesItemNode = function (feature) {
   return item;
 };
 
-//
+// Возвращает domElement <img>, фотография жилья с необходимым scr
+// @photoSrc - строка, url картинки
 var createPhotoNode = function (photoSrc) {
   var item = document.createElement('img');
   item.src = photoSrc;
@@ -269,7 +268,8 @@ var createPhotoNode = function (photoSrc) {
   return item;
 };
 
-//
+// Возвращает очищенный (без дочерних элементов) domElement
+// node - domElement, у которого необходимо удалить все дочерние элементы
 var eraseNode = function (node) {
   while (node.firstChild) {
     node.removeChild(node.firstChild);
@@ -278,13 +278,13 @@ var eraseNode = function (node) {
   return node;
 };
 
-//
+// Возвращает domElement блок объявления
+// @template - domElement (разметка объявления)
+// @adv - объект, содержащий информацию объявления, созданный функцией createAd
 var createCardNode = function (template, adv) {
   var newNode = template.cloneNode(true);
 
-  //console.log(newNode.querySelector('.popup__title'));
   newNode.querySelector('.popup__title').textContent = adv.offer.title;
-
   newNode.querySelector('.popup__text--address').textContent = adv.offer.address;
   newNode.querySelector('.popup__text--price').textContent = adv.offer.price.toString() + ' ₽/ночь';
   switch (adv.offer.type) {
@@ -292,16 +292,16 @@ var createCardNode = function (template, adv) {
       var housingType = 'Дворец';
       break;
     case 'flat':
-      var housingType = 'Квартира';
+      housingType = 'Квартира';
       break;
     case 'house':
-      var housingType = 'Дом';
+      housingType = 'Дом';
       break;
     case 'bungalo':
-      var housingType = 'Бунгало';
+      housingType = 'Бунгало';
       break;
     default:
-      var housingType = 'Неизвестно';
+      housingType = 'Неизвестно';
       break;
   }
   newNode.querySelector('.popup__type').textContent = housingType;
@@ -309,7 +309,7 @@ var createCardNode = function (template, adv) {
   newNode.querySelector('.popup__text--time').textContent = 'Заезд после ' + adv.offer.checkin + ', выезд до ' + adv.offer.checkout;
 
   var featuresFragment = document.createDocumentFragment();
-  for (var i = 0; i < adv.offer.features.length; i++) {
+  for (i = 0; i < adv.offer.features.length; i++) {
     featuresFragment.appendChild(createFeaturesItemNode(adv.offer.features[i]));
   }
   var featuresNode = newNode.querySelector('.popup__features');
@@ -331,9 +331,12 @@ var createCardNode = function (template, adv) {
   return newNode;
 };
 
+
+// Получаем шаблон карточки объявления
+// Создаем карточку с информацией из первого элемента из списка объявлений
 var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
 var card = createCardNode(cardTemplate, advertisements[0]);
 map.insertBefore(card, filterMap);
 
-
+// Отображаем карту
 document.querySelector('.map').classList.remove('map--faded');
